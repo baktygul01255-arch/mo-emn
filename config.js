2065d2ddfc6ac02849ca942ea0bf33721312f166
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-21 20:48:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.PLAN_CONFIG = {
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbzhmp0P52s5qmF27_ghMRDmUfl1NnlC2uNK4ZrYZMYr47R3Z4e2Lr3h_RZ-rS-AtPFu/exec",
 
   // Обычная ссылка на Google Документ, Таблицу, Презентацию или PDF на Google Диске
-  qualityCompositionUrl: "",
+  
 
   // Необязательно: ссылка на общую папку с отчётами для руководителя МО
   reportsFolderUrl: "https://drive.google.com/drive/folders/1sScjDzNLsLub15cI5jnsCXe_Aerna-qW?usp=sharing"
