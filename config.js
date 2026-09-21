5c4224a831854b7335575b3d9799c2baa52fed5b
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-21 21:17:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Вставьте ссылки между кавычками и сохраните файл.
 window.PLAN_CONFIG = {
   // URL развернутого веб-приложения Google Apps Script, заканчивается на /exec
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzYbWLJf4CoBwkZNgRTGOgGrL0tvYn6zKL1LwQ7HT_5bDXAHvm-m6gtwpIg_xtas6EiFg/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbz-vDG9OHtasMrwbaaCaaDAKTsGkxjLWwVtBqhPdnK-cxjDzB_rwQr2a_KAHzL-kYO8zg/exec",
 
   // Необязательно: ссылка на общую папку с отчётами для руководителя МО
   reportsFolderUrl: ""
