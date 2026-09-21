d606f72f867f22684c92c5d2dcd91df4933ed4b2
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-21 18:48:03 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Настройки сайта МО ЕМН.
+// Вставьте ссылки между кавычками и сохраните файл.
+window.PLAN_CONFIG = {
+  // URL развернутого веб-приложения Google Apps Script, заканчивается на /exec
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzhmp0P52s5qmF27_ghMRDmUfl1NnlC2uNK4ZrYZMYr47R3Z4e2Lr3h_RZ-rS-AtPFu/exec",
+
+  // Обычная ссылка на Google Документ, Таблицу, Презентацию или PDF на Google Диске
+  qualityCompositionUrl: "https://docs.google.com/document/d/1IQfFeNYgxd4HdD4LrWwtHdxDExDZL6qV/edit?usp=sharing&ouid=104722562165761310862&rtpof=true&sd=true",
+
+  // Необязательно: ссылка на общую папку с отчётами для руководителя МО
+  reportsFolderUrl: "https://drive.google.com/drive/folders/1sScjDzNLsLub15cI5jnsCXe_Aerna-qW?usp=sharing"
+};
