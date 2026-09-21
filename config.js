b1f2be0d1904e0700963ee872766b9264a918c83
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-21 21:03:15 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -2,11 +2,8 @@
 // Вставьте ссылки между кавычками и сохраните файл.
 window.PLAN_CONFIG = {
   // URL развернутого веб-приложения Google Apps Script, заканчивается на /exec
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzhmp0P52s5qmF27_ghMRDmUfl1NnlC2uNK4ZrYZMYr47R3Z4e2Lr3h_RZ-rS-AtPFu/exec",
-
-  // Обычная ссылка на Google Документ, Таблицу, Презентацию или PDF на Google Диске
-  
+  appsScriptUrl: "",
 
   // Необязательно: ссылка на общую папку с отчётами для руководителя МО
-  reportsFolderUrl: "https://drive.google.com/drive/folders/1sScjDzNLsLub15cI5jnsCXe_Aerna-qW?usp=sharing"
+  reportsFolderUrl: ""
 };
